3485c6131dc26f768b49a87d0afdfed1c5ddcc67
chenmeiling12 <108569295+chenmeiling12@users.noreply.github.com>
2024-01-31 11:14:47 +0800

    update

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -1,8 +1,9 @@
 import { Route, Routes } from "react-router-dom";
 import { Home } from "@/pages/home";
-
-export const Router = () => (
-  <Routes>
-    <Route path="/" element={<Home />} />
-  </Routes>
-);
+export const Router = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
+};
