b5c0654a3406ca7599afc6beecee318b9a813c5c
chenmeiling12 <108569295+chenmeiling12@users.noreply.github.com>
2024-02-06 16:05:23 +0800

    add library

--- web/src/pages/classify/index.tsx
+++ web/src/pages/classify/index.tsx
@@ -1,3 +1,101 @@
+import { Button, Input, Select } from "antd";
+import TextArea from "antd/es/input/TextArea";
+
 export const Classify = () => {
-  return <div>Classify</div>;
+  const handleChange = (value: string) => {
+    console.log(`selected ${value}`);
+  };
+
+  return (
+    <div>
+      <div>問答庫{">"}分類配置</div>
+      <div className="p-6">
+        <div>分類配置</div>
+        <div className="flex float-left flex-wrap">
+          <div className="flex items-center w-[50%]">
+            <div className="w-1/6">分类名称</div>
+            <Input placeholder="Basic usage" style={{ width: "50%" }} />
+          </div>
+          <div className="flex items-center w-[50%]">
+            <div className="w-1/6">分类所属</div>
+            <Select
+              defaultValue="all"
+              style={{ width: "50%" }}
+              onChange={handleChange}
+              options={[
+                { value: "all", label: "全部" },
+                { value: "IS TK", label: "IS TK" },
+                { value: "HR TK", label: "HR TK" },
+                { value: "RS", label: "RS" },
+              ]}
+            />
+          </div>
+          <div className="flex items-center w-[50%]">
+            <div className="w-1/6">分类第一负责人</div>
+            <Select
+              defaultValue="all"
+              style={{ width: 250 }}
+              onChange={handleChange}
+              options={[
+                { value: "all", label: "全部" },
+                { value: "IS TK", label: "IS TK" },
+                { value: "HR TK", label: "HR TK" },
+                { value: "RS", label: "RS" },
+              ]}
+            />
+          </div>
+          <div className="flex w-[50%] pt-10">
+            <div className="w-32"> 分类次要负责人</div>
+            <div className="flex flex-wrap w-[80%]">
+              <Select
+                defaultValue="all"
+                style={{ width: "250px", marginRight: 20 }}
+                onChange={handleChange}
+                options={[
+                  { value: "all", label: "全部" },
+                  { value: "IS TK", label: "IS TK" },
+                  { value: "HR TK", label: "HR TK" },
+                  { value: "RS", label: "RS" },
+                ]}
+              />
+              <Select
+                defaultValue="all"
+                style={{ width: "250px", marginRight: 20 }}
+                onChange={handleChange}
+                options={[
+                  { value: "all", label: "全部" },
+                  { value: "IS TK", label: "IS TK" },
+                  { value: "HR TK", label: "HR TK" },
+                  { value: "RS", label: "RS" },
+                ]}
+              />
+              <Select
+                defaultValue="all"
+                style={{ width: "250px", marginRight: 20, marginTop: 40 }}
+                onChange={handleChange}
+                options={[
+                  { value: "all", label: "全部" },
+                  { value: "IS TK", label: "IS TK" },
+                  { value: "HR TK", label: "HR TK" },
+                  { value: "RS", label: "RS" },
+                ]}
+              />
+            </div>
+          </div>
+          <div className="flex w-1/2 pt-10">
+            <div className="w-1/6">分类说明</div> <TextArea rows={4} />
+          </div>
+          <div className="w-full flex pt-10 justify-around">
+            <div className=" w-1/6 flex justify-around">
+              <Button>保存配置</Button>
+              <Button>取消</Button>
+            </div>
+            <Button type="primary" danger>
+              删除分类
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
--- web/src/pages/library/index.tsx
+++ web/src/pages/library/index.tsx
@@ -1,3 +1,169 @@
+import { routerList } from "@/routes";
+import {
+  Breadcrumb,
+  Button,
+  Input,
+  Select,
+  Space,
+  Table,
+  TableProps,
+} from "antd";
+import { useNavigate } from "react-router-dom";
+
+export interface IManProps {
+  key: string;
+  order: number;
+  name: string;
+  num: number;
+  belong: string;
+  man: string;
+  state: number;
+}
 export const Library = () => {
-  return <div>Library</div>;
+  const handleChange = (value: string) => {
+    console.log(`selected ${value}`);
+  };
+  const navigate = useNavigate();
+
+  const manData: IManProps[] = [
+    {
+      key: "1",
+      order: 1,
+      name: "新人培訓",
+      num: 9,
+      belong: "HR TK",
+      man: "ABC.D",
+      state: 1,
+    },
+    {
+      key: "2",
+      order: 2,
+      name: "雲桌面",
+      num: 3,
+      belong: "IS TK",
+      man: "CREAM.Y",
+      state: 2,
+    },
+    {
+      key: "3",
+      order: 3,
+      name: "財務類問題",
+      num: 0,
+      belong: "HR TK",
+      man: "CHEESE.S",
+      state: 2,
+    },
+  ];
+  const manColumns: TableProps<IManProps>["columns"] = [
+    {
+      title: "編號",
+      dataIndex: "order",
+      align: "center",
+    },
+    {
+      title: "分類名稱",
+      dataIndex: "name",
+      align: "center",
+    },
+    {
+      title: "問題數",
+      dataIndex: "num",
+      align: "center",
+    },
+    {
+      title: "分類所屬",
+      dataIndex: "belong",
+      align: "center",
+    },
+    {
+      title: "負責人",
+      dataIndex: "man",
+      align: "center",
+    },
+    {
+      title: "狀態",
+      dataIndex: "state",
+      align: "center",
+      render: (text: number) => {
+        return text === 1 ? <div>啟用</div> : <div>禁用</div>;
+      },
+    },
+    {
+      title: "操作",
+      key: "action",
+      align: "center",
+      render: (_, record) => (
+        <Space size="middle">
+          <Button
+            onClick={() => {
+              console.log("分類配置");
+              navigate("/library/classify");
+            }}
+          >
+            分類配置
+          </Button>
+          <Button
+            onClick={() => {
+              console.log("詳情");
+              // message.success("刪除成功!");
+            }}
+          >
+            詳情
+          </Button>
+        </Space>
+      ),
+    },
+  ];
+  return (
+    <div>
+      <div>问答库</div>
+      <div className="p-11">
+        <div className="flex justify-between">
+          <div>
+            <Input placeholder="請輸入關鍵詞搜索" style={{ width: "12rem" }} />
+            <Button className="mx-6">查詢</Button>
+          </div>
+          <div className="flex">
+            <div className="mx-6">
+              狀態：
+              <Select
+                defaultValue="all"
+                style={{ width: "5rem" }}
+                onChange={handleChange}
+                options={[
+                  { value: "all", label: "全部" },
+                  { value: 1, label: "啟用" },
+                  { value: 2, label: "禁用" },
+                ]}
+              />
+            </div>
+            <div className="mx-6">
+              分類所屬：
+              <Select
+                defaultValue="all"
+                style={{ width: 120 }}
+                onChange={handleChange}
+                options={[
+                  { value: "all", label: "全部" },
+                  { value: "IS TK", label: "IS TK" },
+                  { value: "HR TK", label: "HR TK" },
+                  { value: "RS", label: "RS" },
+                ]}
+              />
+            </div>
+
+            <Button type="primary" className="mx-6 bg-[#1677ff]">
+              新增分類
+            </Button>
+          </div>
+        </div>
+        <Table
+          columns={manColumns}
+          dataSource={manData}
+          bordered
+          className="mt-10"
+        />
+      </div>
+    </div>
+  );
 };
